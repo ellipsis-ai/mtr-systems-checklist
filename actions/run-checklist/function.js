9ce64b496714453d35f6023c0315b6352917b907
Andrew Catton <>
2018-05-16 18:28:32 +0000

    Some new questions and tweaks from Jess

--- actions/run-checklist/function.js
+++ actions/run-checklist/function.js
@@ -1,4 +1,4 @@
-function(emitters, quickConnects, gutters, lightsOnAuto, ecLevel, ellipsis) {
+function(emitters, quickConnects, gutters, growLightsOnAuto, ecLevel, overheadLights, sandFilterGauge, ellipsis) {
   const EllipsisApi = require('ellipsis-api');
 const api = new EllipsisApi(ellipsis);
 
@@ -9,8 +9,10 @@ MTR checklist for channel <#${channel}> has been completed by <@${user}>:
 ${checkFor(emitters)}   Emitters flowing
 ${checkFor(quickConnects)}   Quick connects connected
 ${checkFor(gutters)}   Gutters draining
-${checkFor(lightsOnAuto)}   Lights on auto
+${checkFor(growLightsOnAuto)}   Grow lights on auto
 ${checkFor(ecLevel)}   EC level on Anderson meter < 2.25
+${checkFor(overheadLights)}   Overhead room lights only on during production activities (M,W,F)
+${checkFor(sandFilterGauge)}   Both red and green arrows on sand filter gauge at 10
 `;
 
 const channels = ["farm-ops-announce"].filter(ea => ea != channel);
